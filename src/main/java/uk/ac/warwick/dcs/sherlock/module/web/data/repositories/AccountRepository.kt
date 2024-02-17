77badf4524e898868f894ce3135eedafa172843d
Xanonymous <trusaidlin@gmail.com>
2024-02-17 08:01:07 +0000

    refactor: 💡 migrate repositories to kotlin

---
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/web/data/repositories/AccountRepository.kt
@@ -0,0 +1,18 @@
+package uk.ac.warwick.dcs.sherlock.module.web.data.repositories
+
+import org.springframework.data.jpa.repository.JpaRepository
+import uk.ac.warwick.dcs.sherlock.module.web.data.models.db.Account
+
+/**
+ * The database repository storing the account details
+ */
+interface AccountRepository : JpaRepository<Account?, Long?> {
+    /**
+     * Find the account with the supplied email
+     *
+     * @param email the email to find the account for
+     *
+     * @return the account found
+     */
+    fun findByEmail(email: String?): Account?
+}
