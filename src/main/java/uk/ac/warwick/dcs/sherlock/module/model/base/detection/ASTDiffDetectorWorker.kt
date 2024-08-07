34534991ac03f78ccce5cd7a2455c5050c8fc225
Xanonymous <trusaidlin@gmail.com>
2024-08-07 01:14:41 +0100

    refactor: 💡 make class final

--- src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/ASTDiffDetectorWorker.kt
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/ASTDiffDetectorWorker.kt
@@ -8,14 +8,14 @@ import tw.xcc.gumtree.model.operations.Action
 import uk.ac.warwick.dcs.sherlock.api.model.detection.DetectorWorker
 import uk.ac.warwick.dcs.sherlock.module.model.base.postprocessing.ASTDiffResult
 
-open class ASTDiffDetectorWorker(
+class ASTDiffDetectorWorker(
     private val treePair: Pair<GumTree, GumTree>,
     parent: ASTDiffDetector,
 ) : DetectorWorker<ASTDiffResult>(parent) {
     private var editScript: List<Action>? = null
-    private val result by lazy { ASTDiffResult(editScript) }
+    private val result by lazy { ASTDiffResult(editScript, parent.params) }
 
-    final override fun execute() {
+    override fun execute() {
         if (treePair.first == treePair.second) {
             return
         }
