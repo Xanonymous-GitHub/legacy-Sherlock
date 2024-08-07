0d8d23ddb1b8226d682eccbc0a413b3925097b38
Xanonymous <trusaidlin@gmail.com>
2024-08-07 14:07:02 +0100

    refactor: 💡 provide source file ref to result

--- src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/ASTDiffDetectorWorker.kt
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/ASTDiffDetectorWorker.kt
@@ -5,15 +5,23 @@ import kotlinx.coroutines.runBlocking
 import tw.xcc.gumtree.DiffCalculator
 import tw.xcc.gumtree.model.GumTree
 import tw.xcc.gumtree.model.operations.Action
+import uk.ac.warwick.dcs.sherlock.api.component.ISourceFile
 import uk.ac.warwick.dcs.sherlock.api.model.detection.DetectorWorker
 import uk.ac.warwick.dcs.sherlock.module.model.base.postprocessing.ASTDiffResult
 
 class ASTDiffDetectorWorker(
     private val treePair: Pair<GumTree, GumTree>,
+    private val sourcePair: Pair<ISourceFile, ISourceFile>,
     parent: ASTDiffDetector,
 ) : DetectorWorker<ASTDiffResult>(parent) {
     private var editScript: List<Action>? = null
-    private val result by lazy { ASTDiffResult(editScript, parent.params) }
+    private val result by lazy {
+        ASTDiffResult(
+            editScript,
+            parent.params,
+            sourcePair
+        )
+    }
 
     override fun execute() {
         if (treePair.first == treePair.second) {
--- src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/postprocessing/ASTDiffResult.kt
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/postprocessing/ASTDiffResult.kt
@@ -1,12 +1,14 @@
 package uk.ac.warwick.dcs.sherlock.module.model.base.postprocessing
 
 import tw.xcc.gumtree.model.operations.Action
+import uk.ac.warwick.dcs.sherlock.api.component.ISourceFile
 import uk.ac.warwick.dcs.sherlock.api.model.postprocessing.AbstractModelTaskRawResult
 import uk.ac.warwick.dcs.sherlock.module.model.base.detection.ASTDiffDetector
 
 data class ASTDiffResult(
     val editScript: List<Action>? = null,
-    val detectorParams: ASTDiffDetector.Params
+    val detectorParams: ASTDiffDetector.Params,
+    val sourcePair: Pair<ISourceFile, ISourceFile>
 ) : AbstractModelTaskRawResult() {
     override fun isEmpty(): Boolean = editScript == null
 
